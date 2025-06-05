f6d66dc427720a388d2a00d4e56a2fe0275c9d5c
Winston Fassett <winston.fassett@gmail.com>
2025-06-05 00:01:23 -0500

    cp

--- src/components/MessageItem.svelte
+++ src/components/MessageItem.svelte
@@ -207,7 +207,6 @@
           {/if}
           
           <!-- Edit/Delete buttons (only for user's own messages) -->
-          )
           {#if isCurrentUser}
             <button
               class="rounded p-1.5 text-gray-500 transition-colors hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-dark-300 dark:hover:text-gray-300"
